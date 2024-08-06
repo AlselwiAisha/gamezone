7354eca89d2bed3863082cc8c435e9c99124e18a
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-08-06 21:11:42 +0300

    Update reviewDetails.js

---
+++ screens/reviewDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function ReviwDetails() {
+    return (
+        <View style={styles.container}>
+        <Text>Reviw Details Screen</Text>
+        </View>
+    );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+    },
+});
