717ccbfb31792d1f69ada4408d20fc60d343310e
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-08-07 20:59:03 +0300

    Update the code with drawer.screen

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,20 +1,23 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
-import About from '../screens/about';
 
 const Stack = createStackNavigator();
 
 function HomeStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
-        <Stack.Screen name="About" component={About} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator >
+      <Stack.Screen
+        name="Home GameZone"
+        component={Home}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="ReviewDetails"
+        component={ReviewDetails}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
   );
 }
 
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text ,TouchableOpacity} from "react-native";
 import { globalStyles } from "../styles/global";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function ReviewDetails({ route }) {
+export default function ReviewDetails({ route, navigation }) {
 
     const { title } = route.params;
     const { body } = route.params;
     const { rating } = route.params;
     return (
         <View style={globalStyles.container}>
+            <TouchableOpacity  onPress={() => navigation.goBack()}>
+               <Ionicons name="arrow-back" size={24} color="black" />
+            </TouchableOpacity> 
             <Text>{title}</Text>
             <Text>{body}</Text>
             <Text>{rating}</Text>
+          
         </View>
     );
 }
