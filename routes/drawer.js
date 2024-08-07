8bb0cb6e9d5486496e9fc610ad04e450029444b4
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-08-07 20:58:22 +0300

    Create drawer.js

---
+++ routes/drawer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeStack from './homeStack';
+import About from '../screens/about';
+
+const Drawer = createDrawerNavigator();
+
+function RootDrawerNavigator() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStack} />
+        <Drawer.Screen name="About" component={About} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default RootDrawerNavigator;
