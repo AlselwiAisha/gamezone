6f25292eb06d21a1f45baa33bf122571c722818e
AlselwiAisha <123330794+AlselwiAisha@users.noreply.github.com>
2024-08-09 20:31:20 +0300

    Add the form to add new reviews

--- screens/home.js
+++ screens/home.js
@@ -1,50 +1,64 @@
-import React , { useState }from "react";
-import { StyleSheet, View, Text, FlatList, TouchableOpacity ,Modal} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { globalStyles } from "../styles/global";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Card from "../shareds/card";
+import ReviewForm from "./reviewForm";
 
 export default function Home({ navigation }) {
-    const [modalOpen,setModalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
     const [review, setReview] = useState([
         { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
         { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
         { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
     ]);
 
-  
+    const addReview = (review) => {
+        review.key = Math.random().toString();
+        setReview((currentReview) => {
+            return [review, ...currentReview];
+        });
+        setModalOpen(false);
+    }
+
     return (
         <View style={globalStyles.container}>
-            <Modal visible={modalOpen} animationType="slide" >
-            <Ionicons 
-            name="close" 
-            style={[styles.addNew, styles.closeModel]}
-            color="black" 
-            onPress={()=>setModalOpen(false)}/>
-                <View style={styles.modelContent}>
-                    <Text style={globalStyles.titleText}>Add new</Text>
-                </View>
+            <Modal visible={modalOpen} animationType="slide">
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.model}>
+                        <Ionicons
+                            name="close"
+                            color="black"
+                            onPress={() => setModalOpen(false)}
+                            style={styles.closeModel}
+                        />
+                        <View style={styles.modelContent}>
+                            <ReviewForm addReview={addReview} />
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
             </Modal>
             <Ionicons
-             name="add"
-             color="black"
-             onPress={()=> setModalOpen(true)} 
-             style={styles.addNew}/>
-                
-         <FlatList 
-            data={review}
-            renderItem={({ item }) => (
-                <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('ReviewDetails', item)}>
-                 <Card> 
-                      <Text style={globalStyles.titleText}>{ item.title }</Text>
-                 </Card>
-                 </TouchableOpacity>
-            )}  />
+                name="add"
+                color="black"
+                onPress={() => setModalOpen(true)}
+                style={styles.addNew}
+            />
+            <FlatList
+                data={review}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Card>
+                            <Text>{item.title}</Text>
+                        </Card>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     );
- }
+}
 
- styles = StyleSheet.create({   
+const styles = StyleSheet.create({
     model: {
         flex: 1,
         padding: 10,
